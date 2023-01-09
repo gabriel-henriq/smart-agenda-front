b0036504eff691a924e64493467b854d452e10c2
Gabriel Henrique <gabrielhcm01@gmail.com>
2023-01-08 21:02:16 -0300

    chore: improve crud and try to fix resource component

--- src/components/basicScheduler.tsx
+++ src/components/basicScheduler.tsx
@@ -1,24 +1,22 @@
 import * as React from 'react';
 import Paper from '@mui/material/Paper';
 import {amber, deepOrange,  purple} from "@mui/material/colors";
-import {AppointmentModel, ChangeSet, EditingState, SchedulerDateTime, ViewState,Resource} from '@devexpress/dx-react-scheduler';
+import {AppointmentModel, ChangeSet,  SchedulerDateTime, Resource, EditingState, ViewState} from '@devexpress/dx-react-scheduler';
 
 import {
     AppointmentForm,
     Appointments,
     AppointmentTooltip,
     DayView,
+    DragDropProvider,
+    EditRecurrenceMenu,
     CurrentTimeIndicator,
     Resources,
-    Scheduler, DragDropProvider, EditRecurrenceMenu,
+    Scheduler,
 } from '@devexpress/dx-react-scheduler-material-ui';
 
 interface CustomAppointment extends AppointmentModel {
-    id: number | string;
-    roomId: number;
-    professorId: number;
-    tabletId: number;
-    student: string;
+    roomId?: string;
 }
 
 const initialAppointments: Array<CustomAppointment> = [
@@ -26,21 +24,15 @@ const initialAppointments: Array<CustomAppointment> = [
         startDate: new Date(),
         endDate: new Date(),
         title: 'Meeting',
-        id: 1,
-        roomId: 1,
-        professorId: 1,
-        tabletId: 1,
-        student: 'Student 1',
+        id: "1",
+        room: "Sala 1",
     },
     {
         startDate: new Date(),
         endDate: new Date(),
         title: 'Lecture',
-        id: 2,
-        roomId: 2,
-        professorId: 2,
-        tabletId: 2,
-        student: 'Student 2',
+        id: "2",
+        room: "Sala 1",
     },
 ];
 const resourcesData: Array<Resource> = [
@@ -49,114 +41,68 @@ const resourcesData: Array<Resource> = [
         title: "Sala",
         instances: [
             {
-                id: 1,
+                id: "0",
                 text: "Sala 1",
-                color: amber
+                color: amber,
             },
-            {
-                id: 2,
-                text: "Sala 2",
-                color: deepOrange
-            },
-            {
-                id: 3,
-                text: "Sala 3",
-                color: purple
-            },
-        ]
-    },
-    {
-        fieldName: "professorId",
-        title: "Professor",
-        instances: [
-            {
-                id: 1,
-                text: "Daniele",
-                color: amber
-            },
-            {
-                id: 2,
-                text: "Alberto",
-                color: deepOrange
-            },
-            {
-                id: 3,
-                text: "Lucas",
-                color: purple
-            },
-        ]
-    },
-    {
-        fieldName: "tabletId",
-        title: "Tablet",
-        instances: [
-            {
-                id: 1,
-                text: "Tablet 1",
-                color: amber
-            },
-            {
-                id: 2,
-                text: "Tablet 2",
-                color: deepOrange
-            },
-            {
-                id: 3,
-                text: "Tablet 3",
-                color: purple
-            },
-        ]
+            // {
+            //     id: "1",
+            //     text: "Sala 2",
+            //     color: deepOrange,
+            // },
+            // {
+            //     id: "2",
+            //     text: "Sala 3",
+            //     color: purple,
+            // },
+        ],
     },
 ];
 
+
 const Schedulerr: React.FC = () => {
     const [currentDate, setCurrentDate] = React.useState<SchedulerDateTime>(new Date());
-    const [appointments, setAppointments] = React.useState<CustomAppointment[]>(initialAppointments);
-    const [editingAppointment, setEditingAppointment] = React.useState<Partial<AppointmentModel>>();
+    const [appointments, setAppointments] = React.useState<Array<CustomAppointment>>(initialAppointments);
+    const [editingAppointment, setEditingAppointment] = React.useState<Partial<CustomAppointment>>();
 
-    const handleEditingAppointmentChange = (appointment: Partial<AppointmentModel>) => {
+    const handleEditingAppointmentChange = (appointment: Partial<CustomAppointment>) => {
         setEditingAppointment(appointment);
     };
 
     const handleCommitChanges = (changes: ChangeSet) => {
         if (changes.added) {
             // Create a new appointment
-            const newAppointment = changes.added[0];
-            appointments.push(newAppointment);
+            const newAppointment = changes.added as CustomAppointment;
+            newAppointment.id = Date.now();
+            setAppointments([...appointments, newAppointment]);
         }
+
         if (changes.changed) {
             // Update an existing appointment
             const changedAppointmentId = Object.keys(changes.changed)[0];
             const changedAppointment = changes.changed[changedAppointmentId];
             const updatedAppointments = appointments.map((appointment) => {
-                if (appointment.id === changedAppointmentId) {
+                if (appointment.id?.toString() === changedAppointmentId) {
                     return { ...appointment, ...changedAppointment };
                 }
                 return appointment;
             });
             setAppointments(updatedAppointments);
         }
-        try {
-            if (changes.deleted) {
-                // Delete an appointment
-                const deletedAppointmentId = changes.deleted;
-                const updatedAppointments = appointments.filter(
-                    (appointment) => appointment.id !== deletedAppointmentId
-                );
-                setAppointments(updatedAppointments);
-            }
-        } catch (err) {
-            console.log(err)
+
+        if (changes.deleted) {
+            // Delete an appointment
+            const deletedAppointmentId = changes.deleted;
+            const updatedAppointments = appointments.filter(
+                (appointment) => appointment.id !== deletedAppointmentId
+            );
+            setAppointments(updatedAppointments);
         }
-
     };
-
-
 
     return (
         <Paper>
             <Scheduler
-                height={600}
                 data={appointments}
             >
                 <EditingState
@@ -169,7 +115,10 @@ const Schedulerr: React.FC = () => {
                     currentDate={currentDate}
                     onCurrentDateChange={setCurrentDate}
                 />
-                <DayView/>
+                <DayView
+                    startDayHour={20}
+                    endDayHour={23}
+                />
                 <Appointments/>
                 <AppointmentTooltip
                     showCloseButton
